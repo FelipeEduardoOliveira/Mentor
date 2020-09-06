55ccfbcc495793698d207996fe824e9c9751d7eb
Felipe Oliveira <feoliveira.eduardo@gmail.comgit config --global user.email feoliveira.eduardo@gmail.comgit config --global user.email feoliveira.eduardo@gmail.com>
2020-09-05 21:40:36 -0300

    Subindo build

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,6 @@ import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import Home from './pages/home';
-<<<<<<< HEAD
-=======
-import Login from './pages/login';
-import Register from './pages/register';
-import Profile from './pages/profile';
-
->>>>>>> bb329cf49dcd3f9e7aa3800eb1c4815d1dcd43c8
 
 function Routes(){
    
@@ -16,12 +9,6 @@ function Routes(){
             <BrowserRouter>
                 <Switch>
                     <Route exact path='/' component={Home} />
-<<<<<<< HEAD
-=======
-                    <Route exact path='/login' component={Login} />
-                    <Route exact path={'/register'} component={Register}/>
-                    <Route exact path={'/profile'} component={Profile}/>
->>>>>>> bb329cf49dcd3f9e7aa3800eb1c4815d1dcd43c8
                 </Switch>
             </BrowserRouter>
 
